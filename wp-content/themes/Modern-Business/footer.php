c6da8ae9ec944f72436a44c6d240ee66f19e3168
devansh-twinspark <devansh.mishra@twinspark.co>
2024-05-09 15:34:02 +0530

    updates in themes

--- wp-content/themes/Modern-Business/footer.php
+++ wp-content/themes/Modern-Business/footer.php
@@ -1,14 +1,24 @@
-  <!-- Footer-->]
+  <!-- Footer-->
         <footer class="bg-dark py-4 mt-auto">
             <div class="container px-5">
                 <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-                    <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; Your Website 2023</div></div>
+                    <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; <?php echo bloginfo();?> <?php echo date('Y');?></div></div>
                     <div class="col-auto">
-                        <a class="link-light small" href="#!">Privacy</a>
-                        <span class="text-white mx-1">&middot;</span>
-                        <a class="link-light small" href="#!">Terms</a>
-                        <span class="text-white mx-1">&middot;</span>
-                        <a class="link-light small" href="#!">Contact</a>
+                        <?php
+                            // Get the menu items for the menu location 'primary-menu'
+                            $menu_items = wp_get_nav_menu_items('footer-menu');
+
+                            if ($menu_items):
+                            ?>
+                                <?php foreach ($menu_items as $menu_item): ?>
+                                        <a class="link-light small" href="<?php echo esc_url($menu_item->url); ?>">
+                                            <?php echo esc_html($menu_item->title); ?>
+                                        </a>
+                                        <span class="text-white mx-1">&middot;</span>
+                                <?php endforeach; ?>
+                            <?php
+                            endif;
+                            ?>
                     </div>
                 </div>
             </div>
